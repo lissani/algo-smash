6e9ea94d4a8225215f30e40bee2186f173051892
lissani <jhs040617@sogang.ac.kr>
2026-01-25 07:08:03 -0500

    [Silver IV] Title: 큐, Time: 212 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.cc
+++ 백준/Silver/10845. 큐/큐.cc
@@ -1,90 +1,49 @@
-#include <iostream>
-#include <string>
+// push, top, size, empty, pop
+
+# include <iostream>
+# include <queue>
+# include <string>
 
 using namespace std;
 
-class queue {
-private:
-    int arr[10000]; // 큐의 원소를 저장하는 배열
-    int q_size;
-    int front_pos;
-    int back_pos;
-public:
-    queue() {
-        q_size = 0;
-        front_pos = 0;
-        back_pos = 0;
-    }
-    void push(int elem) {
-        arr[++back_pos] = elem; // index 1부터 저장
-        q_size++;
-        if (front_pos == 0) front_pos = 1;
-    }
-    int pop() {
-        if (q_size != 0) {
-            int top = arr[front_pos];
-            front_pos++;
-            q_size--;
-            return top;
-        }
-        return -1;
-    }
-    int size() {
-        return q_size;
-    }
-    void empty() {
-        if(q_size == 0){
-            cout << 1 << endl;
-        }
-        else cout << 0 << endl;
-    }
-    void front() {
-        if (q_size == 0) {
-            cout << -1 << endl;
-        }
-        else cout << arr[front_pos] << endl;
-    }
-    void back() {
-        if (q_size == 0) {
-            cout << -1 << endl;
-        }
-        else cout << arr[back_pos] << endl;
-    }
-};
+int main() {
+    int n;
+    cin >> n;
 
-int main(void) {
-    int N;
-    cin >> N;
+    queue<int> q;
 
-    string order;
+    string cmd;
     int num;
 
-    queue q;
+    for (int i=0; i<n; i++){
+        cin >> cmd;
 
-    for (int i = 0; i < N; i++) {
-        cin >> order;
-
-        if (order == "push") {
+        if (cmd=="push"){
             cin >> num;
             q.push(num);
         }
-        if (order == "pop") {
-            num = q.pop();
-            cout << num << endl;
+        else if (cmd=="front"){
+            if (q.empty()) cout << -1 << endl;
+            else cout << q.front() << endl;
         }
-        if (order == "size") {
-            num = q.size();
-            cout << num << endl;
+	else if (cmd=="back"){
+            if (q.empty()) cout << -1 << endl;
+            else cout << q.back() << endl;
         }
-        if (order == "empty") {
-            q.empty();
+        else if (cmd=="size"){
+            cout << q.size() << endl;
         }
-        if (order == "front") {
-            q.front();
+        else if (cmd=="empty"){
+            cout << q.empty() << endl;
         }
-        if (order == "back") {
-            q.back();
+        else if (cmd=="pop"){
+            if (q.empty()) cout << -1 << endl;
+            else {
+                cout << q.front() << endl;
+                q.pop();
+            }
+            // cout << s.top() << endl;
+            // s.pop();
         }
     }
-    return 0;
 }
